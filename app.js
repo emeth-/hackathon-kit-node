c7ca6d7e1aacc8f8ec374cc3862fd0a303759d9b
david cushman <davidrcushman@gmail.com>
2017-06-05 11:13:49 -0700

    github webhook to jira issue done

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
-var github = require('./github')
+// var app = express();
+var github = require('./github');
+var request = require('request');
+var http = require('http');
+var createRQHandler = require('./rq');
+var createHandler = require('github-webhook-handler')
+var githubPullRequestHandler = createHandler({ path: '/gh-pr-update', secret: '' })
+var jiraWebhookHandler = createRQHandler({ path: '/issue' });
+var _ = require('underscore');
+
+http.createServer(function (req, res) {
+    var url = req.url.split('?')[0];
+    if (url === '/gh-pr-update') {
+        githubPullRequestHandler(req, res, function (err) {
+            res.statusCode = 404
+            res.end('no such location')
+        })
+    } else if (url === '/issue') {
+        jiraWebhookHandler(req, res, function (err) {
+            res.statusCode = 404
+            res.end('no such location')
+        })
+    } else {
+        //wut
+    }
+}).listen(8081);
+
+githubPullRequestHandler.on('pull_request', function (data) {
+    if (data.payload) {
+        console.log('webhook caught from github for pullRequest: ', data.payload.pull_request.html_url);
+        var matches = data.payload.pull_request.title.match(/TOP\-\d{1,}/g);
+        if (matches !== null) {
+            _.each(matches, function (match) {
+                updateIssue(match, data.payload.pull_request.html_url);
+            });
+        }
+    }
+});
+
+jiraWebhookHandler.on('jira-issue', function (data) {
+    console.log('* caught', arguments);
+    _.each(data.data.changelog.items, function (change) {
+        debugger;
+        if (change.field === 'status') {
+            if (change.fromString === 'Merge To Dev' && change.toString === 'Project Manager Review') {
+                //make call to pull request to set dev label on it
+            } else if (change.fromString === 'Merge to Staging' && change.toString === 'Stakeholder Review') {
+                //make call to pull request to set staging label on it
+            } else if (change.fromString === 'Merge to Production' && change.toString === 'Completed') {
+                //prod case
+            }
+        }
+    });
+});
+
 /*
   TOPOPPS' internal Jira / Github integration.
 
@@ -10,22 +63,83 @@ var github = require('./github')
 
 */
 
+//
+// app.use( bodyParser.json() );
+//
+// app.post('/gh-pr-update',  function (req, res) {
+//     console.log(arguments);
+// });
+// app.post('/issue',  function (req, res) {
+//
+//     var rawData = req.body.issue;
+//     var action = req.body.issue_event_type_name;
+//     var issueData = {
+//         "issueId": rawData.key,
+//         "pullRequest": rawData.fields.customfield_10201,
+//         "status": rawData.fields.status.name
+//     };
+//
+//     github.repeat(issueData);
+//
+//     res.end("Success!");
+// });
+// const server = app.listen(8081);
 
-app.use( bodyParser.json() )
+var user = '',
+    password = '',
+    secret = '';
 
-app.post('/issue',  function(req, res){
+var getIssue = function (issueNumber) {
+    var options = {
+        type: 'GET',
+        url: 'https://topopps.atlassian.net/rest/api/2/issue/' + issueNumber,
+        headers: {
+            'Authorization': 'Basic ' + new Buffer(user + ':' + password).toString('base64'),
+            'Content-Type': 'application/json'
+        }
+    };
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            debugger;
+            console.log('there', arguments);
+        }
+    });
+};
 
-  var rawData = req.body.issue
-  var action = req.body.issue_event_type_name
-  var issueData = {
-    "issueId": rawData.key,
-    "pullRequest": rawData.fields.customfield_10201,
-    "status": rawData.fields.status.name
-  }
+var updateIssue = function (issueNumber, value) {
+    /*
+    curl -D- -u fred:fred -X PUT --data {see below} -H "Content-Type: application/json" http://kelpie9:8081/rest/api/2/issue/QA-31
 
-  github.repeat(issueData);
-
-  res.end("Success!")
-});
-
-var server = app.listen(8081)
+    {
+        "fields" : {
+            "customfield_10200" :
+            {"value" : "Test 1"}
+            ,
+            "customfield_10201" :
+            {"value" : "Value 1"}
+        }
+    }
+    */
+    var options = {
+        method: 'PUT',
+        json: true,
+        url: 'https://topopps.atlassian.net/rest/api/2/issue/' + issueNumber,
+        headers: {
+            'Authorization': 'Basic ' + new Buffer(user + ':' + password).toString('base64'),
+            'Content-Type': 'application/json'
+        },
+        body: {
+            fields: {
+                customfield_10201: value
+            }
+        }
+    };
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log('there', arguments);
+        }
+    });
+}
+console.log('here');
+//
+// getIssue('TOP-66');
---
+++ rq.js
@@ -0,0 +1,84 @@
+//rq.js
+const EventEmitter = require('events').EventEmitter,
+    inherits = require('util').inherits,
+    crypto = require('crypto'),
+    bl = require('bl'),
+    bufferEq = require('buffer-equal-constant-time')
+
+
+function signBlob(key, blob) {
+    return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex')
+}
+
+
+function create(options) {
+    if (typeof options != 'object')
+        throw new TypeError('must provide an options object')
+    var events
+
+    if (typeof options.events == 'string' && options.events != '*')
+        events = [options.events]
+
+    else if (Array.isArray(options.events) && options.events.indexOf('*') == -1)
+        events = options.events
+
+    // make it an EventEmitter, sort of
+    handler.__proto__ = EventEmitter.prototype
+    EventEmitter.call(handler)
+
+    return handler
+
+
+    function handler(req, res, callback) {
+        if (req.url.split('?').shift() !== options.path || req.method !== 'POST')
+            return callback()
+
+        function hasError(msg) {
+            res.writeHead(400, {
+                'content-type': 'application/json'
+            })
+            res.end(JSON.stringify({
+                error: msg
+            }))
+
+            var err = new Error(msg)
+
+            handler.emit('error', err, req)
+            callback(err)
+        }
+
+
+        req.pipe(bl(function (err, data) {
+            if (err) {
+                return hasError(err.message)
+            }
+
+            var obj
+
+            try {
+                obj = JSON.parse(data.toString())
+            } catch (e) {
+                return hasError(e)
+            }
+
+            res.writeHead(200, {
+                'content-type': 'application/json'
+            })
+            res.end('{"ok":true}')
+            var event = 'jira-issue';
+            var emitData = {
+                event: event,
+                protocol: req.protocol,
+                host: req.headers['host'],
+                url: req.url,
+                data: obj
+            };
+debugger;
+            handler.emit(event, emitData)
+            handler.emit('*', emitData)
+        }))
+    }
+}
+
+
+module.exports = create
